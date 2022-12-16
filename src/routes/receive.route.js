0da2b2c214c19fc962b42b475dad892f151f4d53
0xlkda <hello@thealemazing.com>
2022-12-17 05:23:25 +0700

    decoupling tsv parsing & download zip links

--- src/routes/receive.route.js
+++ src/routes/receive.route.js
@@ -15,12 +15,37 @@ function promisesPartition(array) {
   return [passes, fails]
 }
 
-function downloadZipFiles(orders, onDone) {
+function processUploadedFile(request, response, next) {
+  const log = request.log
+  const file = request.file
+  const filename = file.originalname
+
+  log(`JOB::${filename}::[START PROCESSING]`)
+  tsvParsers
+    .exec(file)
+    .then((orders) => {
+      request.orders = orders
+      next()
+    })
+    .catch(next)
+
+  response.ok(`Processing ${file.originalname}`)
+}
+
+function downloadZipFiles(request, response, next) {
+  const log = request.log
+  const file = request.file
+  const filename = file.originalname
+  const orders = request.orders
+
+  if (!orders || !orders.length) {
+    return log(`JOB::${filename}::[NO ORDER FOUND]`)
+  }
+
   const promises = orders.map((order) => {
     const zipUrl = order['customized-url']
     const orderId = order['order-id']
-    const report = (label, message) =>
-      onDone(`OrderID ${orderId} [${label}]: ${message}`)
+    const report = (label, msg) => log(`OrderID ${orderId} [${label}]: ${msg}`)
 
     return new Promise((resolve, reject) => {
       downloaders
@@ -36,27 +61,18 @@ function downloadZipFiles(orders, onDone) {
     })
   })
 
-  return Promise.allSettled(promises)
-}
+  Promise.allSettled(promises).then((promises) => {
+    const [passes, fails] = promisesPartition(promises)
+    log(`JOB::${filename}::[PASSED ${passes.length}, FAILED ${fails.length}]`)
+  })
 
-function processUploadedFile(request, response, next) {
-  const log = request.log
-  const file = request.file
-  const filename = file.originalname
-
-  log(`JOB::${filename}::[START PROCESSING]`)
-  tsvParsers
-    .exec(file)
-    .then((orders) => downloadZipFiles(orders, log))
-    .then((promises) => {
-      const [passes, fails] = promisesPartition(promises)
-      log(`JOB::${filename}::[PASSED ${passes.length}, FAILED ${fails.length}]`)
-    })
-    .catch(next)
-
-  response.ok(`Processing ${file.originalname}`)
+  next()
 }
 
 module.exports = express
   .Router()
-  .post('/', [FileResolver.single('orders'), processUploadedFile])
+  .post('/', [
+    FileResolver.single('orders'),
+    processUploadedFile,
+    downloadZipFiles,
+  ])
